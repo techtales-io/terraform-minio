e3dfcb64449303a776fdcde0c1b138d6b290f481
Nils Müller <tyriis@users.noreply.github.com>
2025-05-11 22:03:15 +0200

    feat(cnpg): setup bucket and user in nas.techtales.io

--- terraform/nas.techtales.io/backend.tf
+++ terraform/nas.techtales.io/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                      = "terraform"
-    key                         = "techtales/minio/terraform.tfstate"
+    key                         = "nas.techtales.io/minio/terraform.tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
